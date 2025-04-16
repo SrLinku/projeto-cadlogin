b2135959f9cffd42376a509dcde6c3888bf78060
Lincoln <lincoln.alves.de.oliveira@gmail.com>
2025-04-15 22:02:06 -0300

    Exibição de dados da tabela
    Colocamos o botão de salvar o nome do cadastro e assim salvando e exibindo na tabela alem disso na tabela adicionamos dois botões:
    1 - Editar, o botão de editar oferece a opção de editar o nome cadastrado
    2 - Excluir, o botão exclui o nome cadastrado

--- main.js
+++ main.js
@@ -10,3 +10,38 @@ function acessar(){
         window.location.href = "cadastro.html"
     }
 }
+
+//cria variavel do tipo array
+var dadosLista = [];
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        crialista();
+        console.log(dadosLista);
+        document.getElementById('nomeUser').value = "";
+    }else{
+        alert("Por favor informe o nome do cadastro")
+    }
+}
+
+// Função para preencher a lista de cadastro
+function crialista(){
+    let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+    for(let i = 0; i<= (dadosLista.length -1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+// Função para excluir nome da lista
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+    document.getElementById('tabela').deleteRow(i);
+}
+
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+}
